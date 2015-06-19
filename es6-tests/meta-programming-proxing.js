9f0be86d243bef74e50b08993cd7b317e0b18b57
itspoma <itspoma@gmail.com>
2015-06-19 23:46:34 +0300

    added Meta-Programming tests (dummies)

---
+++ es6-tests/meta-programming-proxing.js
@@ -0,0 +1,16 @@
+"use strict"
+
+// let target = {
+//     foo: "Welcome, foo"
+// }
+
+// let proxy = new Proxy(target, {
+//     get (receiver, name) {
+//         return name in receiver ? receiver[name] : `Hello, ${name}`
+//     }
+// })
+
+// console.log(proxy)
+
+// proxy.foo   === "Welcome, foo"
+// proxy.world === "Hello, world"
