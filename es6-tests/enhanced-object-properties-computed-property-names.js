29eff90f4a6205492711ae643c6eda43cb5acb37
itspoma <itspoma@gmail.com>
2015-06-19 23:39:44 +0300

    added Enhanced Object Properties tests

---
+++ es6-tests/enhanced-object-properties-computed-property-names.js
@@ -0,0 +1,11 @@
+"use strict"
+
+// let foo = () => 5
+
+// let obj = {
+//     foo: "bar",
+//     [ "prop_" + foo() ]: 42
+// }
+
+// console.log(obj)
+// console.log('passed')
---
+++ es6-tests/enhanced-object-properties.js
@@ -0,0 +1,8 @@
+"use strict"
+
+let x = 5, y = 10
+let obj = { x, y }
+
+console.assert(obj.x === 5)
+console.assert(obj.y === 10)
+console.log('passed')
