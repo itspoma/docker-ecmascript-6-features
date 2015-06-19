8bd7ac7fc030b0246b06a29fecf313722fe03429
itspoma <itspoma@gmail.com>
2015-06-19 23:48:58 +0300

    added Promises tests (almost dummies)

---
+++ es6-tests/promises-usage.js
@@ -0,0 +1,13 @@
+"use strict"
+
+function msgAfterTimeout (msg, who, timeout) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => resolve(`${msg} Hello ${who}!`), timeout)
+    })
+}
+msgAfterTimeout("", "Foo", 100).then((msg) =>
+    msgAfterTimeout(msg, "Bar", 200)
+).then((msg) => {
+    console.assert(msg === "Hello Foo! Hello Bar!")
+    console.log('passed')
+})
