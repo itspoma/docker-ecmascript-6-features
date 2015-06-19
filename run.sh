0ed1df51a5b45a8787f7419943f52207a5537cdd
itspoma <itspoma@gmail.com>
2015-06-19 23:46:41 +0300

    enabled all harmony flags

--- run.sh
+++ run.sh
@@ -7,7 +7,29 @@ docker run -d --name es6 -v $PWD:/shared -it iojs:2
 docker exec -ti es6 \
     find ./shared/es6-tests/ -name "*.js" \
       -exec echo ">> {}" \;\
-      -exec iojs --harmony --es_staging --harmony_classes --harmony_object_literals --harmony_sloppy --harmony_scoping --harmony_arrow_functions --harmony_shipping --harmony_modules {} \;\
+      -exec iojs \
+        --es_staging \
+        --harmony \
+        --harmony_shipping \
+        --harmony_modules \
+        --harmony_arrays \
+        --harmony_array_includes \
+        --harmony_regexps \
+        --harmony_arrow_functions \
+        --harmony_proxies \
+        --harmony_sloppy \
+        --harmony_unicode \
+        --harmony_unicode_regexps \
+        --harmony_computed_property_names \
+        --harmony_rest_parameters \
+        --harmony_tostring \
+        --harmony_numeric_literals \
+        --harmony_strings \
+        --harmony_scoping \
+        --harmony_templates \
+        --harmony_classes \
+        --harmony_object_literals \
+        {} \;\
       -exec echo "" \;
 
 docker rm -f es6 >/dev/null 2>&1
